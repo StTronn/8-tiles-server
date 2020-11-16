9961ff169c8b7e81bbc77f1714fd5f2ae1ee1ad3
rishav <rishav.21m@gmail.com>
2020-11-17 00:47:30 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import _ from "lodash";
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import Room from "./room";
+import socketio from "socket.io";
+import http from "http";
+
+dotenv.config();
+
+let app = express();
+app.use(cors());
+let server = http.createServer(app);
+let io = socketio(server, {
+  cors: {
+    origin: true,
+  },
+});
+app.use(cors());
+
+let rooms = {};
+
+io.on("connection", (socket) => {
+  socket.on("joinRoom", ({ username, roomId }) => {
+    let room = rooms[roomId] ? rooms[roomId] : new Room(roomId);
+    rooms[roomId] = room;
+
+    room.addUser({ username, id: socket.id });
+    socket.join(roomId);
+    if (Object.keys(room.users).length == 2) {
+      io.to(roomId).emit("init");
+    }
+
+    console.log("joinRoom");
+  });
+
+  socket.on("disconnecting", () => {
+    const roomId = [...socket.rooms][1];
+    if (socket.id && roomId) {
+      rooms[roomId].deleteUser(socket.id);
+      if (_.isEmpty(rooms[roomId].users, true)) rooms = _.omit(rooms, roomId);
+      console.log("leaving room");
+    }
+  });
+});
+
+app.get("/", (req, res) => res.send("server running"));
+
+server.listen("8000", () => {
+  console.log("Listening to port 8000");
+});
---
+++ room.js
@@ -0,0 +1,39 @@
+import _ from "lodash";
+
+export default class Room {
+  constructor(id) {
+    this.id = id;
+    this.intialBoard = {};
+    this.users = {};
+    this.start = false;
+    this.gameOver = false;
+  }
+
+  addUser({ username, id }) {
+    if (Object.keys(this.users).length === 2) return;
+    const userObj = {
+      id,
+      username,
+      grid: this.intialBoard,
+      could_be_won: false,
+      time: 0,
+      solving: false,
+    };
+    this.users[id] = userObj;
+  }
+
+  deleteUser(id) {
+    console.log({ id });
+    if (_.has(this.users, id)) {
+      this.users = _.omit(this.users, id);
+    }
+  }
+
+  updateUser({ userObj }) {
+    this.users = { ...this.users, ...{ [userObj.id]: userObj } };
+  }
+
+  isGameOver() {
+    return false;
+  }
+}
